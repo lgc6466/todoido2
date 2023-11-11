86e4398bed3508e8cf303eff50cb1fcf15eefb1c
yj-yj98 <hyj505505@wku.ac.kr>
2023-11-12 04:50:35 +0900

    로그인 수정

--- app/src/main/java/com/example/todoido/LoginActivity.java
+++ app/src/main/java/com/example/todoido/LoginActivity.java
@@ -1,4 +1,49 @@
 package com.example.todoido;
 
-public class LoginActivity {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.card.MaterialCardView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // activity_login은 레이아웃 XML 파일의 이름이어야 합니다.
+
+        EditText editText = findViewById(R.id.ed_id);
+        MaterialCardView cardView = findViewById(R.id.id_cardview);
+
+        cardView.setStrokeColor(Color.parseColor("#FFDA29"));
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    cardView.setStrokeWidth(3);
+                } else {
+                    cardView.setStrokeWidth(1);
+                }
+            }
+        });
+
+        EditText passwordEditText = findViewById(R.id.et_pass);
+        MaterialCardView passwordCardView = findViewById(R.id.pw_cardview);
+
+        passwordCardView.setStrokeColor(Color.parseColor("#FFDA29"));
+        passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    passwordCardView.setStrokeWidth(3);
+                } else {
+                    passwordCardView.setStrokeWidth(1);
+                }
+            }
+        });
+    }
 }
+
