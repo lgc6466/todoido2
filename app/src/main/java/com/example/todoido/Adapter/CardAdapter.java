33ee4ed5df52e895a1a3a0cd72ecedfd7c9d0cf8
lgc6466 <lgc6466@wku.ac.kr>
2023-11-19 23:02:06 +0900

    .

--- app/src/main/java/com/example/todoido/Adapter/CardAdapter.java
+++ app/src/main/java/com/example/todoido/Adapter/CardAdapter.java
@@ -1,7 +1,6 @@
 package com.example.todoido.Adapter;
 
 import android.content.Context;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +15,8 @@ import com.example.todoido.R;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class CardAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
     private ArrayList<CardItem> items;
-    public static final int BUTTON_VIEW_TYPE = 3;
-
-    // context를 멤버 변수로 선언
     private Context context;
 
     public CardAdapter(ArrayList<CardItem> items, Context context) {
@@ -28,107 +24,48 @@ public class CardAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         this.context = context;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return items.get(position).getViewType();
-    }
-
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view;
-        switch (viewType) {
-            case 0:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_month1, parent, false);
-                return new ViewHolder1(view);
-            case 1:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_month2, parent, false);
-                return new ViewHolder2(view);
-            case 2:
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_month3, parent, false);
-                return new ViewHolder3(view);
-        }
-        return null;
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_month1, parent, false);
+        return new CardViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
         CardItem item = items.get(position);
         String content = item.getContent();
 
-        if (holder instanceof ViewHolder1) {
-            ViewHolder1 viewHolder1 = (ViewHolder1) holder;
-            viewHolder1.contentEditText.setText(content);
-            viewHolder1.closeButton.setOnClickListener(v -> removeItem(holder.getAdapterPosition()));
-        } else if (holder instanceof ViewHolder2) {
-            ViewHolder2 viewHolder2 = (ViewHolder2) holder;
-            viewHolder2.contentEditText.setText(content);
-            viewHolder2.closeButton.setOnClickListener(v -> removeItem(holder.getAdapterPosition()));
-        } else if (holder instanceof ViewHolder3) {
-            ViewHolder3 viewHolder3 = (ViewHolder3) holder;
-            viewHolder3.contentEditText.setText(content);
-            viewHolder3.closeButton.setOnClickListener(v -> removeItem(holder.getAdapterPosition()));
-        }
+        holder.contentEditText.setText(content);
+        holder.closeButton.setOnClickListener(v -> removeItem(position));
     }
-
     public static class CardItem {
         private String content;
-        private int viewType;
 
-        public CardItem(String content, int viewType) {
+        public CardItem(String content) {
             this.content = content;
-            this.viewType = viewType;
         }
 
         public String getContent() {
             return content;
         }
-
-        public int getViewType() {
-            return viewType;
-        }
     }
 
-    // 아이템 추가 메소드
     public void addItem(String content) {
-        int viewType = new Random().nextInt(3);
-        items.add(new CardItem(content, viewType));
+        items.add(new CardItem(content));
         notifyItemInserted(items.size() - 1);
     }
 
-    // 아이템 삭제 메소드
     public void removeItem(int position) {
         items.remove(position);
         notifyDataSetChanged();
     }
 
-    static class ViewHolder1 extends RecyclerView.ViewHolder {
+    static class CardViewHolder extends RecyclerView.ViewHolder {
         EditText contentEditText;
         ImageButton closeButton;
 
-        ViewHolder1(View itemView) {
-            super(itemView);
-            contentEditText = itemView.findViewById(R.id.contentEditText);
-            closeButton = itemView.findViewById(R.id.closeButton);
-        }
-    }
-
-    static class ViewHolder2 extends RecyclerView.ViewHolder {
-        EditText contentEditText;
-        ImageButton closeButton;
-
-        ViewHolder2(View itemView) {
-            super(itemView);
-            contentEditText = itemView.findViewById(R.id.contentEditText);
-            closeButton = itemView.findViewById(R.id.closeButton);
-        }
-    }
-
-    static class ViewHolder3 extends RecyclerView.ViewHolder {
-        EditText contentEditText;
-        ImageButton closeButton;
-
-        ViewHolder3(View itemView) {
+        CardViewHolder(View itemView) {
             super(itemView);
             contentEditText = itemView.findViewById(R.id.contentEditText);
             closeButton = itemView.findViewById(R.id.closeButton);
--- app/src/main/java/com/example/todoido/Fragment/MonthFragment.java
+++ app/src/main/java/com/example/todoido/Fragment/MonthFragment.java
@@ -2,9 +2,9 @@ package com.example.todoido.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,16 +20,29 @@ public class MonthFragment extends Fragment {
 
     ArrayList<CardAdapter.CardItem> items = new ArrayList<>();
     CardAdapter adapter;
+    private ViewPager2 monthRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_month, container, false);
 
-        // RecyclerView와 CardAdapter 설정
-        RecyclerView recyclerView = view.findViewById(R.id.monthRecyclerView);
+        monthRecyclerView = view.findViewById(R.id.monthRecyclerView);
+
+        monthRecyclerView.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float v = Math.abs(Math.abs(position) - 1);
+                page.setScaleX(v / 2 + 0.5f);
+                page.setScaleY(v / 2 + 0.5f);
+            }
+        });
+
+        // ViewPager2와 CardAdapter 설정
+        ViewPager2 viewPager = view.findViewById(R.id.monthRecyclerView);
         adapter = new CardAdapter(new ArrayList<>(), getContext());
-        recyclerView.setAdapter(adapter);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewPager.setAdapter(adapter);
 
         // '+' 버튼 설정
         ImageButton addButton = view.findViewById(R.id.addButton);
