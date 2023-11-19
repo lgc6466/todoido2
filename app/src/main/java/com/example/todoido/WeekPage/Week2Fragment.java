97f4836f752d3554ba9ccaf306f7e9bf908704a9
yj-yj98 <hyj505505@wku.ac.kr>
2023-11-20 07:55:33 +0900

    week page DB연결

--- app/src/main/java/com/example/todoido/WeekPage/Week2Fragment.java
+++ app/src/main/java/com/example/todoido/WeekPage/Week2Fragment.java
@@ -4,28 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.todoido.Adapter.WeekAdapter;
 import com.example.todoido.R;
+import com.example.todoido.ViewModel.WeekViewModel;
 
+public class Week2Fragment extends BaseWeekFragment {
+    private RecyclerView weekRecyclerView;
+    private WeekAdapter weekAdapter;
+    private WeekViewModel weekViewModel;
 
-
-public class Week2Fragment extends Fragment {
     ViewPager viewPager;
-    Button buttonPrev, buttonNext;
+    ImageButton buttonPrev, buttonNext;
+    ImageView mainImageView;
+
+    @Override
+    protected String getWeekId() {
+        return "week2";
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_week2, container, false);
 
-        viewPager = getActivity().findViewById(R.id.week_viewPager);
+        weekViewModel = new ViewModelProvider(requireActivity()).get(WeekViewModel.class);
 
+        weekRecyclerView = view.findViewById(R.id.weekrecyclerView);
+        weekAdapter = new WeekAdapter(weekViewModel, "week2");
+        weekRecyclerView.setAdapter(weekAdapter);
+        weekRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        viewPager = getActivity().findViewById(R.id.week_viewPager);
         buttonPrev = view.findViewById(R.id.week2_prev);
         buttonNext = view.findViewById(R.id.week2_next);
+        mainImageView = view.findViewById(R.id.mainImageView);
+
+        setupDialog(mainImageView);
 
         buttonPrev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +66,38 @@ public class Week2Fragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        weekViewModel.getWeekDataList().observe(getViewLifecycleOwner(), weekDataList -> {
+            for (WeekViewModel.WeekData weekData : weekDataList) {
+                if (weekData.getId().equals("week2")) {
+                    weekAdapter.setSelectedEmoji(weekData.getSelectedEmoji());
+                    weekAdapter.setGoals(weekData.getGoals());
+
+                    String selectedEmoji = weekData.getSelectedEmoji();
+                    switch (selectedEmoji) {
+                        case "happy":
+                            mainImageView.setImageResource(R.drawable.happy);
+                            break;
+                        case "smile":
+                            mainImageView.setImageResource(R.drawable.smile);
+                            break;
+                        case "soso":
+                            mainImageView.setImageResource(R.drawable.soso);
+                            break;
+                        case "bad":
+                            mainImageView.setImageResource(R.drawable.bad);
+                            break;
+                        case "angry":
+                            mainImageView.setImageResource(R.drawable.angry);
+                            break;
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todoido/WeekPage/Week4Fragment.java
+++ app/src/main/java/com/example/todoido/WeekPage/Week4Fragment.java
@@ -4,27 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.todoido.Adapter.WeekAdapter;
 import com.example.todoido.R;
+import com.example.todoido.ViewModel.WeekViewModel;
 
-public class Week4Fragment extends Fragment {
+public class Week4Fragment extends BaseWeekFragment {
+    private RecyclerView weekRecyclerView;
+    private WeekAdapter weekAdapter;
+    private WeekViewModel weekViewModel;
 
     ViewPager viewPager;
-    Button buttonPrev, buttonNext;
+    ImageButton buttonPrev, buttonNext;
+    ImageView mainImageView;
+
+    @Override
+    protected String getWeekId() {
+        return "week4";
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_week4, container, false);
 
-        viewPager = getActivity().findViewById(R.id.week_viewPager);
+        weekViewModel = new ViewModelProvider(requireActivity()).get(WeekViewModel.class);
 
+        weekRecyclerView = view.findViewById(R.id.weekrecyclerView);
+        weekAdapter = new WeekAdapter(weekViewModel, "week4");
+        weekRecyclerView.setAdapter(weekAdapter);
+        weekRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        viewPager = getActivity().findViewById(R.id.week_viewPager);
         buttonPrev = view.findViewById(R.id.week4_prev);
         buttonNext = view.findViewById(R.id.week4_next);
+        mainImageView = view.findViewById(R.id.mainImageView);
+
+        setupDialog(mainImageView);
 
         buttonPrev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,4 +68,36 @@ public class Week4Fragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        weekViewModel.getWeekDataList().observe(getViewLifecycleOwner(), weekDataList -> {
+            for (WeekViewModel.WeekData weekData : weekDataList) {
+                if (weekData.getId().equals("week4")) {
+                    weekAdapter.setSelectedEmoji(weekData.getSelectedEmoji());
+                    weekAdapter.setGoals(weekData.getGoals());
+
+                    String selectedEmoji = weekData.getSelectedEmoji();
+                    switch (selectedEmoji) {
+                        case "happy":
+                            mainImageView.setImageResource(R.drawable.happy);
+                            break;
+                        case "smile":
+                            mainImageView.setImageResource(R.drawable.smile);
+                            break;
+                        case "soso":
+                            mainImageView.setImageResource(R.drawable.soso);
+                            break;
+                        case "bad":
+                            mainImageView.setImageResource(R.drawable.bad);
+                            break;
+                        case "angry":
+                            mainImageView.setImageResource(R.drawable.angry);
+                            break;
+                    }
+                }
+            }
+        });
+    }
 }
