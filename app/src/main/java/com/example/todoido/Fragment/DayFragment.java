f3fcb3b3e96372aa64fcff8022d546e17fc2de0d
yj-yj98 <hyj505505@wku.ac.kr>
2023-11-25 17:45:38 +0900

    day db 연결

--- app/src/main/java/com/example/todoido/Fragment/DayFragment.java
+++ app/src/main/java/com/example/todoido/Fragment/DayFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -19,6 +21,10 @@ import com.example.todoido.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DayFragment extends Fragment {
     private BottomSheetBehavior bottomSheetBehavior;
@@ -27,12 +33,17 @@ public class DayFragment extends Fragment {
 
     private boolean isSheetVisible = false;
 
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private final DatabaseReference databaseRef = firebaseUser != null ? FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid()).child("day") : null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_day, container, false);
 
         bottomSheet = view.findViewById(R.id.sheet_day);
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        EditText day_txt = view.findViewById(R.id.day_txt); // day_txt라는 id를 가진 EditText 참조를 가져옵니다.
+        CheckBox smartNotification = view.findViewById(R.id.smart_notification); // smart_notification이라는 id를 가진 CheckBox 참조를 가져옵니다.
 
         spinner = view.findViewById(R.id.spinner);
 
@@ -47,30 +58,27 @@ public class DayFragment extends Fragment {
                     bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                     isSheetVisible = true;
 
-                    // Spinner에 표시할 항목들
                     String[] items = new String[]{"선택 안 함", "5분 전", "10분 전", "30분 전", "1시간 전", "3시간 전"};
 
-                    // Adapter 생성 및 설정
                     ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
                     spinner.setAdapter(adapter);
 
-                    // 항목 선택 이벤트 처리
                     spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                            // 항목이 선택되었을 때 처리할 작업
+
                             String selectedItem = parent.getItemAtPosition(position).toString();
                             Toast.makeText(getActivity(), selectedItem + " 선택됨", Toast.LENGTH_SHORT).show();
                         }
 
                         @Override
                         public void onNothingSelected(AdapterView<?> parent) {
-                            // 아무 항목도 선택되지 않았을 때 처리할 작업
                         }
                     });
                 }
             }
         });
+
         Button timePickerButton = view.findViewById(R.id.timePickerButton);
         Button timePickerButton2 = view.findViewById(R.id.timePickerButton2);
 
@@ -103,6 +111,30 @@ public class DayFragment extends Fragment {
         timePickerButton.setOnClickListener(timePickerClickListener);
         timePickerButton2.setOnClickListener(timePickerClickListener);
 
+        Button submitButton = view.findViewById(R.id.submit_btn);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String startTime = timePickerButton.getText().toString();
+                String endTime = timePickerButton2.getText().toString();
+                String text = day_txt.getText().toString();
+                String spinnerSelection = spinner.getSelectedItem().toString();
+                boolean isChecked = smartNotification.isChecked();
+
+
+                DatabaseReference newChildRef = databaseRef.push();
+                newChildRef.child("startTime").setValue(startTime);
+                newChildRef.child("endTime").setValue(endTime);
+                newChildRef.child("text").setValue(text);
+                newChildRef.child("spinnerSelection").setValue(spinnerSelection);
+                newChildRef.child("isChecked").setValue(isChecked);
+
+                // BottomSheet를 내립니다.
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                isSheetVisible = false;
+            }
+        });
 
         bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -118,7 +150,7 @@ public class DayFragment extends Fragment {
 
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                // 슬라이드 중인 경우 추가적인 작업이 필요하다면 여기에 구현할 수 있습니다.
+
             }
         });
 
